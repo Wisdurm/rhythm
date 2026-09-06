edd883ddaacf64f95ef762af461159e5941d830e
Wisdurm <luukas.kola@gmail.com>
2026-09-06 21:28:54 +0300

    I spent 20 minutes on fixing indentation :D

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Sound.MIDI.File.Load (fromFile)
-import Sound.MIDI.File (getTracks)
-import Sound.MIDI.File.Event ( T(MIDIEvent), ElapsedTime, fromElapsedTime)
-import Sound.MIDI.Message.Channel ( T(Cons), Body(Voice))
-import Sound.MIDI.Message.Channel.Voice ( T(NoteOn), T(NoteOff), fromPitch)
-import Data.EventList.Relative.TimeBody (toPairList)
-import Data.List (sortBy)
-import qualified Data.Map as Map
+import Network.HTTP.Types.Status (status200, status400)
+import Network.HTTP.Types.Method (methodGet)
+import Network.Wai (Application, Request, Response,
+                    responseLBS, pathInfo, requestMethod)
+import Network.Wai.Handler.Warp (run)
+--import Midi (parseFile)
 
-getNotes :: [(ElapsedTime, Sound.MIDI.File.Event.T)] -> [(Int,Int,Int)]
-getNotes track = go track 0 Map.empty
-  where
-    go ((t, (MIDIEvent (Cons _ (Voice (NoteOn p _))))) : xs) acc starts =
-      let pitch = fromPitch p
-          elapsed = fromIntegral . fromElapsedTime $ t
-          time = acc + elapsed
-      in go xs time (Map.insert pitch time starts)
-    go ((t, (MIDIEvent (Cons _ (Voice (NoteOff p _))))) : xs) acc starts =
-      let pitch = fromPitch p
-          elapsed = fromIntegral . fromElapsedTime $ t
-          time = acc + elapsed
-          startTime = Map.findWithDefault 0 pitch starts
-          period = time - startTime
-          rest = go xs time starts
-      in ((pitch, period, startTime):rest)
-    go ((t,_):xs) acc starts =
-      let elapsed = fromIntegral . fromElapsedTime $ t
-          time = acc + elapsed
-      in go xs time starts
-    go _ _ _ = []
+port :: Int
+port = 1234
+
+wrongMethod :: Response
+wrongMethod = responseLBS status400 [] "Wrong methods"
+
+servePath :: Request -> Response
+servePath req =
+  case pathInfo req of
+    ("api":"notes":_)
+      | requestMethod req == methodGet -> responseLBS status200 [] "Notes"
+      | otherwise -> wrongMethod
+    ("api":_) -> responseLBS status200 [] "Api"
+    _ -> responseLBS status200 [] "Unkown"
+
+application :: Application
+application request respond = do
+  let response = servePath request
+  respond response
 
 main :: IO ()
 main = do
-  file <- fromFile "tulip.mid"
-  let track = head . getTracks $ file
-      list = toPairList track
-      notes = getNotes list
-      (ns,nls,nss) = unzip3 . sortBy (\(_,_,s) (_,_,s') -> compare s s') $ notes
-  print ns
-  print nls
-  print nss
+  run port application
