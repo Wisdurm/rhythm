70ac2c12349c584733323eb6d5689bc440ac1833
Wisdurm <luukas.kola@gmail.com>
2026-09-09 19:39:45 +0300

    Api finally works in some way.

--- app/Main.hs
+++ app/Main.hs
@@ -9,11 +9,18 @@ import Network.Wai (Application, Request, Response,
                     responseLBS, pathInfo, requestMethod,
                     getRequestBodyChunk)
 import Network.Wai.Handler.Warp (run)
+import Network.Wai.Parse (parseRequestBodyEx, lbsBackEnd,
+                         defaultParseRequestBodyOptions,
+                           RequestParseException, Param, File,
+                           fileContentType, fileContent)
+import Control.Exception (try)
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T (pack)
+import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Map as Map
 import GHC.Generics
 import Data.Aeson
---import Midi (parseFile)
+import Midi (parseFile)
 
 data ParseMidi = ParseMidi { text :: String }
   deriving (Generic, FromJSON)
@@ -24,11 +31,25 @@ port = 1234
 wrongMethod :: Response
 wrongMethod = responseLBS status400 [] "Wrong methods"
 
+wrongInput :: Response
+wrongInput = responseLBS status400 [] "Wrong input"
+
 parseData :: Request -> IO (Response)
 parseData req = do
-  c <- getRequestBodyChunk req
-  print c
-  return (responseLBS status200 [] (BL.fromStrict c))
+  r <- Control.Exception.try (parseRequestBodyEx
+                              defaultParseRequestBodyOptions
+                              lbsBackEnd req)
+       :: IO (Either RequestParseException ([Param], [File BL.ByteString]))
+  case r of
+    Right (_,((_,file):_)) -> do
+      if (fileContentType file) == "audio/midi" then do
+        let midi = parseFile . fileContent $ file
+        case midi of Nothing -> return wrongInput
+                     Just m -> return (responseLBS status200 []
+                                        (BL.fromStrict (encodeUtf8( T.pack( show  m)))))
+        else return wrongInput
+    Left ex -> return wrongInput
+    Right _ -> return wrongInput
 
 application :: Application
 application req res
--- app/Midi.hs
+++ app/Midi.hs
@@ -1,6 +1,7 @@
 module Midi where
 
-import Sound.MIDI.File.Load (fromFile)
+import Sound.MIDI.File.Load (maybeFromByteList)
+import qualified Sound.MIDI.Parser.Report as Report
 import Sound.MIDI.File (getTracks)
 import Sound.MIDI.File.Event ( T(MIDIEvent), ElapsedTime, fromElapsedTime)
 import Sound.MIDI.Message.Channel ( T(Cons), Body(Voice))
@@ -8,6 +9,7 @@ import Sound.MIDI.Message.Channel.Voice ( T(NoteOn), T(NoteOff), fromPitch)
 import Data.EventList.Relative.TimeBody (toPairList)
 import Data.List (sortBy)
 import qualified Data.Map as Map
+import qualified Data.ByteString.Lazy as BL
 
 getNotes :: [(ElapsedTime, Sound.MIDI.File.Event.T)] -> [(Int,Int,Int)]
 getNotes track = go track 0 Map.empty
@@ -31,11 +33,13 @@ getNotes track = go track 0 Map.empty
       in go xs time starts
     go _ _ _ = []
 
-parseFile :: String -> IO ([Int], [Int], [Int])
-parseFile str = do
-  file <- fromFile str
-  let track = head . getTracks $ file
-      list = toPairList track
-      notes = getNotes list
-      res = unzip3 . sortBy (\(_,_,s) (_,_,s') -> compare s s') $ notes
-  return res
+parseFile :: BL.ByteString -> Maybe ([Int], [Int], [Int])
+parseFile fData =
+  case maybeFromByteList . BL.unpack $ fData of
+    (Report.Cons errors (Left _)) -> Nothing
+    (Report.Cons _ (Right file)) ->
+      let track = head . getTracks $ file
+          list = toPairList track
+          notes = getNotes list
+          res = unzip3 . sortBy (\(_,_,s) (_,_,s') -> compare s s') $ notes
+      in Just $ res
